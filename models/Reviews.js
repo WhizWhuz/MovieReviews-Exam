bd4917ba9d77c907ecc5edf5588ab86686c61ba9
WhizWhuz <jesper_andren94@hotmail.com>
2025-05-26 06:55:13 +0200

    first commit

---
+++ models/Reviews.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const reviewSchema = new mongoose.Schema(
+  {
+    movieId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Movie",
+      required: true,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+      required: true,
+    },
+    comment: String,
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Reviews", reviewsSchema);
